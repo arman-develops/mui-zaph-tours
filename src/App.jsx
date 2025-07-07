953d335ee653086d9ba59cdb441f2be9601ba0b1
Amon Victor <amon.victor.develops@gmail.com>
2025-07-07 20:25:16 +0300

    Added Routing to the page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import Home from "./pages/Home"
+import { Routes, Route } from "react-router-dom"
+import { ThemeProvider } from "./components/theme/ThemeProvider"
+import { Navbar } from "./components/layout/Navbar"
+import { Footer } from "./components/layout/Footer"
+import HomePage from "./pages/HomePage"
+import DestinationsPage from "./pages/DestinationsPage"
+import TripTypesPage from "./pages/TripTypesPage"
+import ContactPage from "./pages/ContactPage"
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <ThemeProvider>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/destinations" element={<DestinationsPage />} />
+            <Route path="/trip-types" element={<TripTypesPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   )
 }
 
